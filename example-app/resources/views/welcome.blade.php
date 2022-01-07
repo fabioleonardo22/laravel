8df7c2d18d708fceb95bf0b3983e2010502443a1
fabio <fabioleonardo1995@gmail.com>
2022-01-07 14:52:28 -0300

    Criando layout com blade

--- example-app/resources/views/welcome.blade.php
+++ example-app/resources/views/welcome.blade.php
@@ -1,51 +1,40 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+@extends('layouts.main')
 
-        <link rel="stylesheet" href="/css/style.css">
-        <script scr="/js/scripts.js"></script>
-       
-    </head>
-    <body>
+@section('tittle', 'HDC Events')
 
-        <h1>algum titulo</h1>
-        @if(10 > 5)
-            <p>a condicao e true</p>
-        @endif
+@section('content')
+
+ <h1>algum titulo</h1>
+@if(10 > 5)
+<p>a condicao e true</p>
+@endif
         
-        <p>{{$nome}}</p>
+<p>{{$nome}}</p>
 
-        @if ($nome == "pedro")
-        <p>o nome e pedro</p>
-        @elseif($nome == "fabio")
-        <p>o nome e {{ $nome }} e ele tem {{ $idade }} anos, e trabalha {{$profissao}}</p>
-        @else
-        <p>o nome nao e pedro</p>
-        @endif
+@if ($nome == "pedro")
+@elseif($nome == "fabio")
+<p>o nome e {{ $nome }} e ele tem {{ $idade }} anos, e trabalha {{$profissao}}</p>
+@else
+<p>o nome nao e pedro</p>
+@endif
 
-        @for($i = 0; $i < count($arr); $i++)
-            <p>{{ $arr[$i] }} - {{ $i }}</p>
-            @if($i == 2)
-            <p>o i e 2</p>
-            @endif
-        @endfor
+@for($i = 0; $i < count($arr); $i++)
+<p>{{ $arr[$i] }} - {{ $i }}</p>
+@if($i == 2)
+<p>o i e 2</p>
+@endif
+@endfor
 
-        @foreach($nomes as $nome)
-            <p> {{ $loop-> index }} </p>
-            <p> {{ $nome }} </p>
-        @endforeach
+@foreach($nomes as $nome)
+<p> {{ $loop-> index }} </p>
+<p> {{ $nome }} </p>
+@endforeach
             
 
-        @php
-            $name = "fabio";
-            echo $name;
-        @endphp
+@php
+$name = "fabio";
+echo $name;
+@endphp
 
-<!-- comentario do html -->
-
-    </body>
-</html>
+@endsection
